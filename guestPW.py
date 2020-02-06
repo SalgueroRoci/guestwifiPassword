a453b5d2e0509211019ee6b9896ce361d13ada65
Rocio Salguero <rsalguer@Rocios-MBP.home>
2020-02-06 08:18:54 -0800

    Fixed bug with chrome driver

--- guestPW.py
+++ guestPW.py
@@ -19,7 +19,7 @@ import requests
 #Function Grab the guest password
 def getGuestPWD():
     #Configure the selenium webdriver using google chrome
-    options = Options()
+    options = Options()  
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome("./chromedriver", options=options)
